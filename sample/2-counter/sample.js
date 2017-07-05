83161a589e40b67fbdaefea4ccb34c93025281d7
Ralf S. Engelschall <rse@engelschall.com>
2017-07-05 17:31:12 +0200

    use two separate client instances

--- sample/2-counter/sample.js
+++ sample/2-counter/sample.js
@@ -1,10 +1,10 @@
 (async () => {
+    const { Server } = require("graphql-io-server")
+    const { Client } = require("graphql-io-client")
 
     /*
     **  ==== SERVER ====
     */
-
-    const { Server } = require("graphql-io-server")
 
     const server = new Server({ url: "http://127.0.0.1:12345/api" })
     server.at("graphql-schema", () => `
@@ -37,24 +37,37 @@
     await server.start()
 
     /*
-    **  ==== CLIENT ====
+    **  ==== CLIENT #1 ====
     */
 
-    const { Client } = require("graphql-io-client")
-
-    const client = new Client({ url: "http://127.0.0.1:12345/api" })
-    await client.connect()
-
-    let subscription = client.query("subscription { counter { value } }").subscribe((result) => {
-        console.log("SUBSCRIPTION", result.data)
+    const client1 = new Client({ url: "http://127.0.0.1:12345/api" })
+    await client1.connect()
+    let subscription = client1.query(`
+        subscription {
+            counter {
+                value
+            }
+        }
+    `).subscribe((result) => {
+        console.log("Client #1: Result:", result.data)
     })
 
-    let handler = async () => {
-        await client.query("mutation { counter { increase { value } } }")
-        let timeout = 1000 * Math.random()
-        setTimeout(handler, timeout)
-    }
-    let timer = setTimeout(handler, 0)
+    /*
+    **  ==== CLIENT #2 ====
+    */
+
+    const client2 = new Client({ url: "http://127.0.0.1:12345/api" })
+    await client2.connect()
+    setInterval(async () => {
+        let result = await client2.query(`
+            mutation {
+                counter {
+                    increase { value }
+                }
+            }
+        `)
+        console.log("Client #2: Result:", result.data)
+    }, 1 * 1000)
 
 })().catch((err) => {
     console.log("ERROR", err)
