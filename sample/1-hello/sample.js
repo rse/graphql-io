33a5e411c96b36d5df04bd452b8bcb0f7231ab1b
Ralf S. Engelschall <rse@engelschall.com>
2017-12-23 00:30:37 +0100

    more debugging

--- sample/1-hello/sample.js
+++ sample/1-hello/sample.js
@@ -5,7 +5,8 @@
     */
 
     const { Server } = require("graphql-io-server")
-    const server = new Server({ url: "http://127.0.0.1:12345" })
+    const server = new Server({ url: "http://127.0.0.1:12345", debug: 9 })
+    server.at("debug", (ev) => console.log(`server [${ev.level}] ${ev.msg}`))
     server.at("graphql-resolver", () => ({
         Root: { hello: [ "hello: String", () => "world" ] }
     }))
@@ -16,7 +17,8 @@
     */
 
     const { Client } = require("graphql-io-client")
-    const client = new Client({ url: "http://127.0.0.1:12345" })
+    const client = new Client({ url: "http://127.0.0.1:12345", debug: 9 })
+    client.at("debug", (ev) => console.log(`client [${ev.level}] ${ev.msg}`))
     await client.connect()
     let result = await client.query("{ hello }")
     await client.disconnect()
