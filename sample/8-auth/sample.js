6d3b8a0470b2474e186bb883ad70ac2ec013aac3
ruzmanz <zoltan.ruzman@msg.group>
2020-06-04 12:50:19 +0200

    sample for authentication

--- sample/8-auth/sample.js
+++ sample/8-auth/sample.js
@@ -7,16 +7,16 @@
     const { Server } = require("graphql-io-server")
     const server = new Server({ url: "http://127.0.0.1:12345", debug: 9 })
     server.at("debug", (ev) => console.log(`server [${ev.level}] ${ev.msg}`))
-	server.at("account-authenticate", (ctx) => {
-		// Check username and password
-		if(ctx.password !== 'secret1') {
-			return (ctx.error = "invalid password")
-		}
+    server.at("account-authenticate", (ctx) => {
+        // Check username and password
+        if(ctx.password !== 'secret1') {
+            return (ctx.error = "invalid password")
+        }
 
-		// assign accountId
-		ctx.accountId = ctx.username
-		return ctx
-	})
+        // assign accountId
+        ctx.accountId = ctx.username
+        return ctx
+    })
     server.at("graphql-resolver", () => ({
         Root: { hello: [ "hello: String", () => "world" ] }
     }))
@@ -29,12 +29,11 @@
     const { Client } = require("graphql-io-client")
     const client = new Client({ url: "http://127.0.0.1:12345", debug: 9 })
     client.at("debug", (ev) => console.log(`client [${ev.level}] ${ev.msg}`))
-	client.at("login-credentials", (credentials) => {
-			credentials.username     = "john"
-            credentials.password     = "secret1"
-            return credentials
-
-	})
+    client.at("login-credentials", (credentials) => {
+        credentials.username     = "john"
+        credentials.password     = "secret1"
+        return credentials
+    })
     await client.connect()
     await client.login()
     let result = await client.query("{ hello }")
